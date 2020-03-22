19b4018db0b3245971d9692c246bf77aadd6902c
James Dudley <jmdudle2@illinois.edu>
2020-03-22 17:16:41 -0500

    Top titles done

--- TopTitlesMapper.py
+++ TopTitlesMapper.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 
 import sys
+from collections import Counter
 
 word_dict = {}
 
 for line in sys.stdin:
-    line.strip()
-    word, count = line.split('\t')
-    word_dict[word] = word_dict.get(word, 0) + int(count)
+    word, count = line.strip().split('\t')
+    count = int(count)
+    word_dict[word] = word_dict.get(word, 0) + count
 
-sort_word_dict = sorted(word_dict, key=lambda x: word_dict.get(x), reverse=False)
+word_dict_items = word_dict.items()
+sorted_word_dict = sorted(word_dict_items, key=lambda x: x[1], reverse=True)
 
-for word in sort_word_dict[:10]:
-    print("%s\t%d" % (word, word_dict.get(word)))
+for word, count in sorted_word_dict[:10]:
+    print("%s\t%d" % (word, count))
--- TopTitlesReducer.py
+++ TopTitlesReducer.py
@@ -4,4 +4,3 @@ import sys
 
 for line in sys.stdin:
     print(line.strip())
-    #print('%s\t%s' % (  ,  ))
